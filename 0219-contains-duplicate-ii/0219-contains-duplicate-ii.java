1426783ba980c14ccf357fb9870be55d1f615141
gowshi1864 <gowshsubramanian@gmail.com>
2025-08-04 14:20:54 +0530

    Time: 1216 ms (5.04%), Space: 59.3 MB (84.15%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,18 @@
+class Solution 
+{
+    public boolean containsNearbyDuplicate(int[] nums, int k) 
+    {
+        int a=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=i+1;j<=i+k && j<nums.length;j++)
+            {
+                if(nums[i]==nums[j])
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
